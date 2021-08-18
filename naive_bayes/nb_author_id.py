c719022a80b4c3d830f3079c72224cbafe45fce6
Jamil Ahmed <jamil.mba4553@iiu.edu.pk>
2021-08-18 21:58:43 +0500

    Create and train a Naive Bayes classifier in naive_bayes/nb_author_id.py. Use it to make predictions for the test set. What is the accuracy?

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,6 +15,8 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,7 +26,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
-
+clf = GaussianNB()
 
 
 ##############################################################
@@ -36,12 +38,17 @@ The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
 
-# t0 = time()
+t0 = time()
 # # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-# t0 = time()
+t0 = time()
 # # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
+
+accuracy = accuracy_score(labels_test, pred)
+print ("The accuracy on the test set is: ",accuracy)
 ##############################################################

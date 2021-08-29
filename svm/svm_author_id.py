16eed45b276d15a6f7dce2dfed430122e7631ab2
Jamil Ahmed <jamil.mba4553@iiu.edu.pk>
2021-08-29 14:59:03 +0500

    Project updated

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,18 +12,31 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+import collections
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
 #########################################################
 ### your code goes here ###
 
+clf = SVC(kernel="rbf", gamma='auto', C=10000)
 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+# print(pred)
 #########################################################
 
 #########################################################
@@ -36,4 +49,12 @@ The Code Below solves that issue, So use this one
 # features_train = features_train[:int(len(features_train)/100)]
 # labels_train = labels_train[:int(len(labels_train)/100)]
 
+
+accuracy = accuracy_score(labels_test, pred)
+print ("The accuracy on the test set is: ",accuracy)
+
+# answer = pred[50]
+answer = collections.Counter(pred)
+# pred.count(1)
+print("Answer is: ", answer)
 #########################################################
